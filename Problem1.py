68d31fc9c4790f2aa7b1350378bd4a68cf063c2f
Erfandarzi <48624909+Erfandarzi@users.noreply.github.com>
2019-10-30 15:36:05 +0330

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -7,17 +7,21 @@ from keras.optimizers import RMSprop
 from keras.datasets import mnist
 from keras.utils import np_utils
 import pandas as pd
-print('hello')
+
+
+#setting parameters
 batch_size = 32
 nb_classes = 10
 nb_epoch = 30
 lr=0.001
-# Load MNIST dataset
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-X_val = X_train[40000:].reshape(20000, 784)
-X_train = X_train[:40000].reshape(40000, 784)
-# X_train = X_train.reshape(60000, 784)
+
+# Load MNIST dataset
+(X_in, y_in), (X_test, y_test) = mnist.load_data()
+
+#Splitting Data
+X_val = X_in[40000:].reshape(20000, 784)
+X_train = X_in[:40000].reshape(40000, 784)
 X_test = X_test.reshape(10000, 784)
 
 
@@ -29,10 +33,13 @@ X_train /= 255
 X_test /= 255
 X_val /= 255
 
+
+#Splitting the labels
 Y_val = np_utils.to_categorical(y_train, nb_classes)[40000:]
 Y_train = np_utils.to_categorical(y_train, nb_classes)[:40000]
-# Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+
 
 # Deep Multilayer Perceptron model
 model = Sequential()
@@ -49,7 +56,7 @@ model.compile(optimizer=RMSprop(lr=lr), loss='categorical_crossentropy', metrics
 model.summary()
 
 # Train
-history = model.fit(X_train, Y_train,nb_epoch=nb_epoch, batch_size=batch_size, verbose=1)
+history = model.fit(X_train, Y_train,validation_data=(X_val,Y_val),nb_epoch=nb_epoch, batch_size=batch_size, verbose=1)
 
 # convert the history.history dict to a pandas DataFrame:     
 hist_df = pd.DataFrame(history.history) 
